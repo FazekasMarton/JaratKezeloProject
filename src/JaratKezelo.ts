ce5e86956f5b5cf46281c49b12fde2d7687d4bd0
FazekasMarton <fazekas.marton16@gmail.com>
2024-06-07 12:43:57 +0200

    class created

---
+++ src/JaratKezelo.ts
@@ -0,0 +1,55 @@
+// src/JaratKezelo.ts
+export class JaratKezelo {
+    private jaratok: Map<string, {
+        repterHonnan: string,
+        repterHova: string,
+        indulas: Date,
+        keses: number
+    }> = new Map();
+
+    ujJarat(jaratSzam: string, repterHonnan: string, repterHova: string, indulas: Date): void {
+        if (this.jaratok.has(jaratSzam)) {
+            throw new Error('A járatszámnak egyedinek kell lennie!');
+        }
+
+        this.jaratok.set(jaratSzam, {
+            repterHonnan,
+            repterHova,
+            indulas,
+            keses: 0
+        });
+    }
+
+    keses(jaratSzam: string, keses: number): void {
+        const jarat = this.jaratok.get(jaratSzam);
+        if (!jarat) {
+            throw new Error('Nem létező járat!');
+        }
+
+        jarat.keses += keses;
+        if (jarat.keses < 0) {
+            throw new Error('A késés összértéke nem lehet negatív!');
+        }
+    }
+
+    mikorIndul(jaratSzam: string): Date {
+        const jarat = this.jaratok.get(jaratSzam);
+        if (!jarat) {
+            throw new Error('Nem létező járat!');
+        }
+
+        const indulas = new Date(jarat.indulas.getTime());
+        indulas.setMinutes(indulas.getMinutes() + jarat.keses);
+        return indulas;
+    }
+
+    jaratokRepuloterrol(repter: string): string[] {
+        const jaratok = [];
+        for (const [jaratSzam, jarat] of this.jaratok.entries()) {
+            if (jarat.repterHonnan === repter) {
+                jaratok.push(jaratSzam);
+            }
+        }
+        return jaratok;
+    }
+}
